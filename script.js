370a285660c019826e6e37ce429f7123b23c8045
Donmark2k <chukwuemekavp@gmail.com>
2022-12-20 06:08:08 +0100

    solve linters error

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const links = Array.from(document.querySelectorAll('.menu-link'));
 const close = document.querySelector('.close');
-const menu = document.querySelector('#mobile-nav')
+const menu = document.querySelector('#mobile-nav');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('inactive');
-})
+  menu.classList.toggle('inactive');
+});
 
 close.addEventListener('click', () => {
-    menu.classList.toggle('inactive');
-})
+  menu.classList.toggle('inactive');
+});
 
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        menu.classList.toggle('inactive');
-    })
-})
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.toggle('inactive');
+  });
+});
