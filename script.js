a4ca2c819ef550b6b344182b8bfcf82853c527da
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2022-12-20 08:40:55 +0100

    Fix linter errors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const links = Array.from(document.querySelectorAll('.menu-link'));
 const close = document.querySelector('.close');
-const menu = document.querySelector('#mobile-nav');
+const menu = document.querySelector('.mobile-nav');
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('inactive');
