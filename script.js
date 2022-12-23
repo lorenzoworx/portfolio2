1a3a727c73b96a67a92ec02232cb0454815e25ff
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2022-12-23 11:22:13 +0100

    Fix modal scrolling

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const hamburger = document.querySelector('.hamburger');
-const links = Array.from(document.querySelectorAll('.menu-link'));
-const close = document.querySelector('.close');
-const menu = document.querySelector('.mobile-nav');
+const hamburger = document.querySelector(".hamburger");
+const links = Array.from(document.querySelectorAll(".menu-link"));
+const close = document.querySelector(".close");
+const menu = document.querySelector(".mobile-nav");
 
-const showModal = Array.from(document.querySelectorAll('.showModal'));
-const closeModal = document.querySelector('.closeModal');
-const popup = document.querySelector('.popSection');
+const showModal = Array.from(document.querySelectorAll(".showModal"));
+const closeModal = document.querySelector(".closeModal");
+const popup = document.querySelector(".popSection");
 
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('inactive');
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("inactive");
 });
 
-close.addEventListener('click', () => {
-  menu.classList.toggle('inactive');
+close.addEventListener("click", () => {
+  menu.classList.toggle("inactive");
 });
 
 links.forEach((link) => {
-  link.addEventListener('click', () => {
-    menu.classList.toggle('inactive');
+  link.addEventListener("click", () => {
+    menu.classList.toggle("inactive");
   });
 });
 
-showModal.forEach(item => {
-  item.addEventListener('click', () => {
-    popup.classList.toggle('inactive')
-  })
-} )
+showModal.forEach((item) => {
+  item.addEventListener("click", () => {
+    popup.classList.toggle("inactive");
+  });
+});
 
-closeModal.addEventListener('click', () => {
-  popup.classList.toggle('inactive');
+closeModal.addEventListener("click", () => {
+  popup.classList.toggle("inactive");
 });
